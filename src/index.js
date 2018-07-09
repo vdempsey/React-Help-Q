d534fd1b68a735a44617f2084da813a7da272f89
Valia Dempsey <valentinaml@gmail.com>
2018-07-09 14:30:48 -0700

    Set up initial file structure for react-help-q project. CDN react.js and react-dom.js. Render initial greeting and live time elements.

---
+++ src/index.js
@@ -0,0 +1,12 @@
+function liveTime() {
+  var greeting = React.createElement('h1', {}, 'Hello, World!');
+  var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
+  var app = React.createElement('div', {}, greeting, clock);
+
+  ReactDOM.render(
+    app,
+    document.getElementById('react-app-root')
+  );
+}
+
+setInterval(liveTime, 1000);
