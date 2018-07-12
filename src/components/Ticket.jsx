13aed3abfdabd500440bb11ba1df2fe41b328a7a
Valia Dempsey <valentinaml@gmail.com>
2018-07-12 12:21:30 -0700

    Install and setup ESLint

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 function Ticket(props){
   return (
     <div>
       <h3>{props.location}</h3>
-    <h3>{props.names}</h3>
-  <p><em>{props.issue}</em></p>
+      <h3>{props.names}</h3>
+      <p><em>{props.issue}</em></p>
       <hr/>
     </div>
   );
