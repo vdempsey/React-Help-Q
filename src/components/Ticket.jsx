efa6fecbd9ff6ef3d891fa14ebfb91aab4056b3b
Valia Dempsey <valentinaml@gmail.com>
2018-07-12 05:07:06 -0700

    Pass, define props for Ticket

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Ticket(){
+function Ticket(props){
   return (
     <div>
-      <h3>3a</h3>
-      <h3>Thato and Haley</h3>
-      <p><em>Firebase will not save record!</em></p>
+      <h3>{props.location}</h3>
+    <h3>{props.names}</h3>
+  <p><em>{props.issue}</em></p>
       <hr/>
     </div>
   );
 }
+
+Ticket.propTypes = {
+  names: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  issue: PropTypes.string
+};
 
 export default Ticket;
