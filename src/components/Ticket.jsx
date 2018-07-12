960db55238f829ad3d60b87646a0f5d075d70471
Valia Dempsey <valentinaml@gmail.com>
2018-07-12 04:50:45 -0700

    Separate content into components

---
+++ src/components/Ticket.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Ticket(){
+  return (
+    <div>
+      <h3>3a</h3>
+      <h3>Thato and Haley</h3>
+      <p><em>Firebase will not save record!</em></p>
+      <hr/>
+    </div>
+  );
+}
+
+export default Ticket;
