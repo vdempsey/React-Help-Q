79c2fdb90de8255d0a3eafc7c59d9dbf9e85cc98
Valia Dempsey <valentinaml@gmail.com>
2018-07-12 05:47:27 -0700

    Set developementserver to host project locally, add hot module replacment for live-updates

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -4,7 +4,7 @@ import Ticket from "./Ticket";
 function TicketList(){
   return (
     <Ticket
-      location="3A"
+      location="12A"
       names="Thato and Haley"
       issue="Firebase will not save record!"/>
   );
