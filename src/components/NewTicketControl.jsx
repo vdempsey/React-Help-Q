207ea0afc13e9e3818c25288b3208694cd4b4c69
Valia Dempsey <valentinaml@gmail.com>
2018-08-02 22:58:00 -0700

    Add onClick event in NewTicketControl, add handleClick event handler

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -7,12 +7,19 @@ class NewTicketControl extends React.Component {
     this.state = {
       formVisibleOnPage: false
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  render() {
+  handleClick(){
+    this.setState({formVisibleOnPage: true});
+    console.log('formVisibleOnPage is currently set to:' + this.state.formVisibleOnPage);
+  }
+
+  render(){
     return (
       <div>
         <p>This is the NewTicketControl component!</p>
+        <strong onClick={this.handleClick}>Click me to change my state!</strong>
       </div>
     );
   }
