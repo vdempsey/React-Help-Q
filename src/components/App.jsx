6a7cb4b5522687eae96635159fc207d69b31f9fb
Valia Dempsey <valentinaml@gmail.com>
2018-08-02 22:06:18 -0700

    Create class based stateful component NewTicketControl, update route in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import TicketList from './TicketList';
-import NewTicketForm from './NewTicketForm';
+import NewTicketControl from './NewTicketControl';
 import Error404 from './Error404';
 
 function App(){
@@ -17,10 +17,9 @@ function App(){
       <Header/>
       <Switch>
         <Route exact path='/' component={TicketList} />
-        <Route path='/newticket' component={NewTicketForm} />
-        <Route component={Error404} /> 
+        <Route path='/newticket' component={NewTicketControl} />
+        <Route component={Error404} />
       </Switch>
-
     </div>
   );
 }
